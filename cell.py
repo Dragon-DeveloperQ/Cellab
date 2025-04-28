499da5e301fe2f6af6d1a09d6eba5b4d24842027
Quroboros <ouroborosq54@gmail.com>
2025-04-28 15:11:56 +0300

    0.2 force control

--- cell.py
+++ cell.py
@@ -5,23 +5,29 @@ config = dotenv_values(".env")
 
 
 class _Cell:
-    def __init__(self, x, y, radius, mass, color):
+    def __init__(self, id, x, y, radius, mass, color):
+        self.__id = id
         self.mass = mass
         self.position = (x, y)
         self.radius = radius
         self.__color = color
 
-        dtype = [('id', 'i4'), ('value', '2i4')]
-        self.__energy = np.array(dtype=dtype)
+        dtype = [('id', 'i4'), ('value', '2f8')]
+        self.__energy = np.array([0, (0, 0)], dtype=dtype)
+    
+    def get_id(self):
+        return self.__id
 
     def get_color(self):
         return self.__color
     
     def add_force(self, id, vector):
-        self.__energy = np.append(self.__energy, (id, vector))
+        new_force = np.array([(id, vector)], dtype=self.__energy.dtype)
+        self.__energy = np.append(self.__energy, new_force)
     
     def change_force(self, id, vector):
         self.__energy[np.where(self.__energy['id'] == id)[0]] = (id, vector)
+        print(self.__energy)
     
     def remove_force(self, id):
         i = np.where(self.__energy['id'] == id)[0]
@@ -29,13 +35,17 @@ class _Cell:
             self.__energy = np.delete(self.__energy, i)
         else:
             print('Не найдена сила, которую нужно удалить')
+    
+    def move(self):
+        for i in self.__energy['value']:
+            self.position += i 
 
 
 class WhiteCell(_Cell):    
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius, int(config["CELL_WHITE_MASS"]), "white")
+    def __init__(self, id, x, y, radius):
+        super().__init__(id, x, y, radius, int(config["CELL_WHITE_MASS"]), "white")
 
 
 class RedCell(_Cell):
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius, int(config["CELL_RED_MASS"]), "red")
+    def __init__(self, id, x, y, radius):
+        super().__init__(id, x, y, radius, int(config["CELL_RED_MASS"]), "red")
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import dotenv_values
 
 import cell
 
+
 # Загрузка конфига
 config = dotenv_values(".env")
 screen_width = int(config["SCREEN_WIDTH"])
@@ -19,26 +20,44 @@ screen = pygame.display.set_mode((screen_width, screen_heigth))
 # Инициализация обьекта времени
 clock = pygame.time.Clock()
 # Запись начального времени выполненния программы
-start_program_time = pygame.time.get_ticks()
+# start_program_time = pygame.time.get_ticks()
 
 
 # Инициализация клеток
 offsetX = screen_width // 2
 offsetY = screen_heigth // 2
 
-cell1 = cell.WhiteCell(0, 0, 10)
-
 cells = np.empty(10, dtype=object)
 for i in range(len(cells)):
-    if i <= 5:
-        cells[i] = cell.WhiteCell(randint(0, screen_width), randint(0, screen_heigth), 5)
+    if i != 1:
+        cells[i] = cell.WhiteCell(i, randint(0, screen_width), randint(0, screen_heigth), 5)
     else:
-        cells[i] = cell.RedCell(randint(0, screen_width), randint(0, screen_heigth), 7)
+        cells[i] = cell.RedCell(i, randint(0, screen_width), randint(0, screen_heigth), 7)
+
+cell1 = cell.WhiteCell(-1, 0, 0, 10)
+cells = np.append(cells, cell1)
+
+
+
+
+
+for i in cells:
+        if i.get_color() == 'white':
+            
+            for j in cells:
+                if j.get_color() == 'red':
+                    vector = ( 1/(abs(i.position[0] - j.position[0])**2), 1/(abs(i.position[1] - j.position[1])**2))
+                    j.add_force(i.get_id(), vector)  
+
+
+
+
 
 
 # Инициализация цикла жизни
 while True:
     screen.fill("black")
+
 
     # Обработка события выхода
     for event in pygame.event.get():
@@ -46,16 +65,33 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # Расчет данны для таймера
-    time = (pygame.time.get_ticks() - start_program_time) / 1000
+
+    # Расчет данных для таймера
+    # time = (pygame.time.get_ticks() - start_program_time) / 1000
     
 
+    # Отрисовка пользовательской клетки (статичной)
     x, y = pygame.mouse.get_pos()
-    #pygame.draw.circle(screen, "white", (math.sin(time)*100 + offsetX, math.cos(time)*100 + offsetY), 10)
+    cell1.position = (x, y)
     pygame.draw.circle(screen, cell1.get_color(), (x, y), cell1.radius)
+
+    
     for i in cells:
+
+        # Измененния сил, действующих на клетку
+        if i.get_color() == 'white':    
+            for j in cells:
+                if j.get_color() == 'red':
+                    vector = ( 1/(i.position[0] - j.position[0]), 1/(i.position[1] - j.position[1]))
+                    j.change_force(i.get_id(), vector)
+                    
+
+        # Выполнить движение клетки с учетом всех сил
+        i.move()
+        # Отрисовать новое положениее клетки
         pygame.draw.circle(screen, i.get_color(), i.position, i.radius)
     
+
     # Обновление кадра
     pygame.display.flip()
     clock.tick(fps)
